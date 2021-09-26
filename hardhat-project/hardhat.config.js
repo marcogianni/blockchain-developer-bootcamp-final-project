c41ab12ce9d0fb742c6c2d2cc868bef5f6138f36
Marco Gianni <gian93ma50@gmail.com>
2021-09-26 17:21:12 +0200

    added hardhat plugins

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -1,4 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-ethers");
+// require('@openzeppelin/contracts');
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-gas-reporter");
+require("hardhat-tracer");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +25,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  // Gas Reporter
+  // etherscan: {
+  //   apiKey: "",
+  // },
+  // gasReporter: {
+  //   currency: "USD",
+  //   coinmarketcap: "",
+  // },
+
+  // to deploy
+  // networks: {
+  //   rinkeby: {
+  //     url: "", //Infura url with projectId
+  //     accounts: [""], // add the account that will deploy the contract (private key)
+  //   },
+  // },
 };
