7e804c69e5167ded1b2d590fbf5897e2df951e27
Marco Gianni <gian93ma50@gmail.com>
2021-10-03 16:07:32 +0200

    created DAI token
    for test purpose

---
+++ hardhat-project/test/general-test.js
@@ -0,0 +1,63 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { time } = require("@openzeppelin/test-helpers");
+
+const MONTH = 2592000;
+
+const displayTime = (unixTime) => {
+  var date = new Date(unixTime * 1000).toLocaleString("it-IT");
+  return date;
+};
+
+describe("Salaries - TEST", () => {
+  let Salaries, salaries, DAIToken, daiToken, owner;
+  let daiTokenAddress, salariesAddress;
+  const provider = ethers.getDefaultProvider();
+
+  let initialTotalSupply;
+  let currentBlock;
+
+  beforeEach(async () => {
+    DAIToken = await ethers.getContractFactory("ERC20");
+    Salaries = await ethers.getContractFactory("Salaries");
+    [
+      owner, // 50 ether
+      addr1, // 0
+      addr2, // 0
+      addr3, // 0
+      addr4, // 0
+      addr5, // 0
+      addr5, // 0
+      addr6, // 0
+      addr7, // 0
+      addr8, // 0
+      addr9, // 0
+      addr10, // 0
+      addr11, // 0
+      addr12, // 0
+      addr13, // 0
+      addr14, // 0
+      addr15, // 0
+      addr16, // 0
+      addr17, // 0
+      addr18, // 1000 ether
+    ] = await ethers.getSigners();
+  });
+
+  describe("Current Block", () => {
+    it("Should be 0", async () => {
+      currentBlock = await time.latest();
+      const currentBlockNumber = await time.latestBlock();
+
+      console.debug(
+        "\t\t\tCurrent Block Number",
+        currentBlockNumber.toString()
+      );
+      console.debug("\t\t\tCurrent Block Timestamp", currentBlock.toString());
+      console.debug(
+        "\t\t\tCurrent Block Time",
+        displayTime(Number(currentBlock.toString()))
+      );
+    });
+  });
+});
