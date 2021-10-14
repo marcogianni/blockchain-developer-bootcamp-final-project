102a50da73697d51c8ae333b499978932ecc5cd3
Marco Gianni <gian93ma50@gmail.com>
2021-10-14 16:26:41 +0200

    added keys

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -24,6 +24,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
